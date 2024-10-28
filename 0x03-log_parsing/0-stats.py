aaa2766807dc8a633dcd4141c5ce985640c383f0
wabkur <wabilitukur.com>
2024-10-28 07:05:12 +0300

    parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,12 +5,12 @@ import sys
 
 def print_msg(dict_sc, total_file_size):
     """
-    Method to print
+    Method to print --
     Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
+        dict sc: dict of the status codes
+        total file size: total of the file
     Returns:
-        Nothing
+        return 0.
     """
 
     print("File size: {}".format(total_file_size))
