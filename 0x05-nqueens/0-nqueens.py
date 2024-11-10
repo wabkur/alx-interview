825fc2b647228b481c3c7aa971223e0fdd5d362a
wabkur <wabilitukur.com>
2024-11-10 23:55:04 +0300

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python3
 import sys
 
-
 def ChessBoard(n: int):
-    """solving N queens problem with Backtracking algorithm.
+    """Solving N queens problem with Backtracking algorithm.
     Args:
         n (int): non-attacking queens to place on board.
 
-    Return:
+    Returns:
         List[List[int]]: list of columns & rows of where queens are placed.
     """
     result = []
 
     def checkBoard(row, col, col_in_row):
-        """Checks if queen can be placed without attacking the other queens"""
+        """Checks if a queen can be placed without attacking other queens."""
         for r in range(row):
             if col_in_row[r] == col or row - r == abs(col - col_in_row[r]):
                 return False
         return True
 
     def saveBoard(col_in_row):
-        """Saves the current state of the board as a solution"""
+        """Saves the current state of the board as a solution."""
         solution = [[i, col_in_row[i]] for i in range(n)]
         result.append(solution)
 
     def placeQueen(row, cols, col_in_row):
-        """Places non-attacking queens on an n x n chessboard"""
-        if row ==n:
+        """Places non-attacking queens on an n x n chessboard."""
+        if row == n:
             saveBoard(col_in_row)
-        return
+            return
         for col in range(n):
             if cols[col] == 0 and checkBoard(row, col, col_in_row):
                 cols[col] = 1
                 col_in_row[row] = col
                 placeQueen(row + 1, cols, col_in_row)
                 cols[col] = 0
-    placeQueen(0, [0]*n, [0]*n)
-    return result
 
+    placeQueen(0, [0] * n, [0] * n)
+    return result
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isdigit() is False:
+    if not sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
     if int(sys.argv[1]) < 4:
